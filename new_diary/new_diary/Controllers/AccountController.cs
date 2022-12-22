f819220ba4bc110fae251e436036cd89c7d111bd
SamuraiKeksik <67840425+SamuraiKeksik@users.noreply.github.com>
2022-12-22 21:49:44 +0300

    Login and logout
    Added "Account" controller with login and logout actions and login view.

---
+++ new_diary/new_diary/Controllers/AccountController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using new_diary.Models;
+using new_diary.Models.ViewModels;
+
+namespace new_diary.Controllers
+{
+    public class AccountController : Controller
+    {
+        SignInManager<MyUser> _signInManager;
+        UserManager<MyUser> _userManager;
+        public AccountController(SignInManager<MyUser> signInManager, UserManager<MyUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public IActionResult Login(string? returnUrl) //Аутентификация юзера
+        {
+            var loginModel = new LoginModel { ReturnUrl = returnUrl ?? "/" };
+            return View(loginModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel login)
+        {
+            if(ModelState.IsValid)
+            {                
+                var user = await _userManager.FindByEmailAsync(login.Email);
+                if(user != null)
+                {
+                    await _signInManager.SignOutAsync();
+                    var result = await _signInManager.PasswordSignInAsync(user, login.Password, login.RememberMe, false);
+                    if (result.Succeeded)
+                        return Redirect(login.ReturnUrl ?? "/");
+                }
+                ModelState.AddModelError(nameof(login.Email), "Invalid Email or password");
+
+            }
+            return View(login);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
--- new_diary/new_diary/Models/ViewModels/LoginModel.cs
+++ new_diary/new_diary/Models/ViewModels/LoginModel.cs
@@ -2,12 +2,14 @@
 
 namespace new_diary.Models.ViewModels
 {
-    public class LoginUserForm
+    public class LoginModel
     {        
         [Required]
         [RegularExpression("^[a-zA-Z0-9_\\.-]+@([a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; }
         [Required]
         public string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public string ReturnUrl { get; set; }
     }
 }
